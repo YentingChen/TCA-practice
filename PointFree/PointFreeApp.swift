bb2f2fc8fa5c14aaa2a2122a5fcd9aa284feab87
Yenting Chen <yeonbo0620@gmail.com>
2024-02-01 07:50:09 +0000

    Initial Commit

---
+++ PointFree/PointFreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointFreeApp.swift
+//  PointFree
+//
+//  Created by Yenting Chen on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PointFreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
